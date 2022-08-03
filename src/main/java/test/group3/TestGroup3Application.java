258a66e747130083135de17390774617648c641a
Rodrigo Gomez Portillo <rodrigo.gomez@eggcooperation.com>
2022-08-03 18:44:46 -0300

    first commit

---
+++ src/main/java/test/group3/TestGroup3Application.java
@@ -0,0 +1,13 @@
+package test.group3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGroup3Application.class, args);
+	}
+
+}
---
+++ src/test/java/test/group3/TestGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.group3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
